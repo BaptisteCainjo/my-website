d6cb1ce17bb6a95dfe73ba7ca683bdae4d481390
Baptiste Cainjo <cainjo.baptiste@orange.fr>
2024-10-09 18:48:32 +0200

    little change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return(
     <>
-    <p>Le site est actuellement en pleine évolution pour une version améliorée.</p>
+    <p style={{ textAlign: 'center' }}>Le site est actuellement en pleine évolution pour une version améliorée.</p>
     </>
   )
 }
