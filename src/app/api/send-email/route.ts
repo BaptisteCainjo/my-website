1a3feec06ace8c4af6973c1d9f66bfc7f485f788
Baptiste Cainjo <cainjo.baptiste@orange.fr>
2024-11-10 11:49:00 +0100

    api key

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend("re_Ly1JAotg_M8Moa43oq3UHdPaGbou3CVb3");
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: Request) {
   const { name, email, message } = await request.json();
