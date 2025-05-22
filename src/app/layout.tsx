c9a1ca69801a8d9f3551f316d5f2d1b52e452c30
Baptiste Cainjo <cainjo.baptiste@orange.fr>
2025-05-22 21:10:30 +0200

    fix: Update portfolio description and reintroduce portfolio entry in JSON data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import Cursor from "@/components/Cursor/Cursor";
 export const metadata: Metadata = {
   title: "Baptiste Cainjo • Développeur Full-stack",
   description:
-    "Site web personnel de Baptiste CAINJO, étudiant en 3eme année de BUT MMI. Ce portfolio contient mes projets, mon parcours ou encore un formulaire de contact.",
+    "Site web personnel de Baptiste CAINJO, développeur Full-Stack chez MMA depuis septembre 2022. Ce portfolio contient mes projets, mon parcours ou encore un formulaire de contact.",
   icons: {
     icon: "./images/favicon.ico",
   },
