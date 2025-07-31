2f7f18874f5931f2074df89a7bab992d4e0a33c7
Baptiste Cainjo <cainjo.baptiste@orange.fr>
2025-07-31 22:19:58 +0200

    fix: font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,13 @@ import BtnUp from "@/components/BtnUp/BtnUp";
 import Cursor from "@/components/Cursor/Cursor";
 import Footer from "@/components/Footer/Footer";
 import "../scss/globals.scss";
+import { Outfit } from "next/font/google";
+
+const outfit = Outfit({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata: Metadata = {
   title: "Baptiste Cainjo • Développeur Full-stack",
@@ -22,7 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
-      <body>
+      <body className={outfit.className}>
         <Cursor />
         <main>{children}</main>
         <BtnUp />
