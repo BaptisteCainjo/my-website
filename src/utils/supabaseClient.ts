66107258ec904882946c8b51197b363ef8f99b4b
Baptiste Cainjo <cainjo.baptiste@orange.fr>
2025-11-27 23:29:39 +0100

    feat: update Supabase client configuration with hardcoded URL and anon key

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = "https://ztecoljjakrlgrkwxciu.supabase.co/";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0ZWNvbGpqYWtybGdya3d4Y2l1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI0MjYxMzAsImV4cCI6MjA2ODAwMjEzMH0.c-wFBowvkTyz2X57JAsuevM_JjACYD_XQiucj0ylfVQ";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
