b0d6115c907146a47181bb7d5e98862184afc785
Baptiste Cainjo <cainjo.baptiste@orange.fr>
2026-01-10 22:41:07 +0100

    fix: correct typo in HOST_ADDRESS constant

--- src/utils/constants/general.tsx
+++ src/utils/constants/general.tsx
@@ -1,2 +1,2 @@
 export const EMAIL = "cainjo.baptiste@orange.fr";
-export const HOST_ADDRESS = "340 S Lemon Ave, Walnut, CA 91789, USAs"
+export const HOST_ADDRESS = "340 S Leemon Ave, Walnut, CA 91789, USAs"
