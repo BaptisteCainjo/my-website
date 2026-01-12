75e2c946c953c225f2ca907e3508168a194457a6
Baptiste Cainjo <cainjo.baptiste@orange.fr>
2026-01-12 18:54:44 +0100

    fix: correct HOST_ADDRESS constant to remove erroneous 's' character

--- src/utils/constants/general.tsx
+++ src/utils/constants/general.tsx
@@ -1,2 +1,2 @@
 export const EMAIL = "cainjo.baptiste@orange.fr";
-export const HOST_ADDRESS = "340 S Lemodn Ave, Walnut, CA 91789, USsA";
+export const HOST_ADDRESS = "340 S Lemodn Ave, Walnut, CA 91789, USA";
